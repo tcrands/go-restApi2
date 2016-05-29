8dc2265982d5885ef6350857049b150e6e0e6923
tcrands <tcrands@gmail.com>
2016-05-29 15:23:00 +0100

    cusotm server

--- handlers.go
+++ handlers.go
@@ -12,13 +12,17 @@ func homePage(w http.ResponseWriter, r *http.Request) {
     fmt.Println("Endpoint Hit: homePage")
 }
 
+//r is a pointer to the http.Request interface
 func getSomething(w http.ResponseWriter, r *http.Request) {
-
+	//:= infers type in a function
 	dataResults := DataResults {
 		DataResult{Id: "1", Name: "Tom", Content:"something blah blah"},
 	}
 
-	fmt.Println("Endpoint Hit: getSomething")
+	fmt.Println("Endpoint Hit: getSomething", r.Host)
+	//w is the response writer
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
 
 	if err := json.NewEncoder(w).Encode(dataResults); err != nil {
         panic(err)
--- main.go
+++ main.go
@@ -3,11 +3,18 @@ package main
 import (
     "log"
     "net/http"
+    "time"
 )
 
 func main() {
     router := NewRouter()
-    log.Fatal(http.ListenAndServe(":8080", router))
+
+    server := &http.Server{
+    	Addr: ":8080",
+    	Handler: router,
+    	ReadTimeout: 10 * time.Second, 
+    	WriteTimeout: 10 * time.Second,
+    	MaxHeaderBytes: 1 << 20,    	
+    }
+    log.Fatal(server.ListenAndServe())
 }
-
-
